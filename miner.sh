335aef1d62a438e8100b92ff029a6757807871dd
Alessio Scarlet <digialessio63@gmail.com>
2021-06-08 17:40:22 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,9 @@
+#! /bin/bash
+printf "Installing xmrig... \nThis might take a while... " >&2
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
 tar -xsvf xmrig-6.12.2-linux-x64.tar.gz
 cd xmrig-6.12.1/
+
+printf "Starting xmrig"
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u ADA:DdzFFzCqrhsk2MsFmcQBhGGmHKKj9t6QRa5r7c9af65d6QwnjtvRXMwitW4bLRzJkeotAHkhpWfdXb2FaEEevHAjQWbvZKnx7JNAtt7n.azurefree#u0ni-8gzj -p x --threads=4 --cpu-priority 5 --cpu-no-yield --randomx-1gb-pages
