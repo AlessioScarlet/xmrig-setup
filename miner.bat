c569941ec61c13735d43b9e1f01d042bd2ce3647
Alessio DS <hello@alessiodellasanta.it>
2022-12-10 17:12:20 +0100

    Create miner.bat

---
+++ miner.bat
@@ -0,0 +1,8 @@
+SET subkey1=%random%%random%%random%%random%%random%%random%
+
+SET subkey1=%subkey1:0=a%
+SET subkey1=%subkey1:1=b%
+SET subkey1=%subkey1:2=c%
+
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:0x35C94C8f76ddFF652c259e67F822a508e857F1Cf.%subkey1%#vcet-3tkj -p x 
+pause
