7e0ec42bd6698da600bd6fc6cb6656fbf2287a0d
Alessio DS <hello@alessiodellasanta.it>
2022-12-11 10:05:13 +0100

    Update miner.bat

--- miner.bat
+++ miner.bat
@@ -4,5 +4,5 @@ SET subkey1=%subkey1:0=a%
 SET subkey1=%subkey1:1=b%
 SET subkey1=%subkey1:2=c%
 
-xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:0x35C94C8f76ddFF652c259e67F822a508e857F1Cf.%subkey1%#vcet-3tkj -p x 
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:0x35C94C8f76ddFF652c259e67F822a508e857F1Cf.%subkey1%#vcet-3tkj -p x --threads=4 --cpu-priority 5 --cpu-no-yield --randomx-1gb-pages
 pause
